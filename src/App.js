4b3cc93795a4936ee60b2205834b50502fad6627
Thomas Pendleton <thomas6576@yahoo.com>
2022-03-24 16:09:44 -0700

    added to app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import logo from './logo.svg'
 import { Component } from 'react'
 import './App.css'
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           This is my react app
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class App extends Component {
   constructor() {
@@ -29,28 +7,72 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      filteredMonsters: [],
+      searchString: '',
     }
+    console.log('constructor')
+    // this.filter = this.filter.bind(this)
   }
 
   componentDidMount() {
+    console.log('component did mount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((data) =>
         this.setState(
           () => {
-            return { monsters: data }
+            return { monsters: data, filteredMonsters: data }
           },
           () => {
-            console.log(this.state.monsters);
+            // console.log(this.state.monsters)
           }
         )
       )
   }
 
+  // filter(e){
+  // //   console.log(e.target.value)
+  // this.setState(
+  //   () => {
+  //     // console.log(this.state.monsters);
+  //   return this.state.monsters.filter(monster => {
+  //     // console.log(monster.name)
+  //     return monster.name.indexOf(e.target.value) !== -1})
+  //   },
+  //   () => {
+  //
+  //   }
+  // )
+  // }
+
   render() {
+    console.log('render')
+    const filteredMonsters = this.state.monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(this.state.searchString)
+    )
+    console.log(filteredMonsters);
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            const searchString = e.target.value.toLocaleLowerCase()
+
+            this.setState(
+              () => {
+                return { searchString }
+              },
+              () => {
+                console.log(this.state.monsters, this.state.searchString)
+              }
+            )
+          }}
+        />
+
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>
