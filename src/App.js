184a6faab9040f68510d5b29e9d18e5e139d5c4b
Thomas Pendleton <thomas6576@yahoo.com>
2022-03-23 21:50:30 -0700

    starting new react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           This is my react app
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState(
+          () => {
+            return { monsters: data }
+          },
+          () => {
+            console.log(this.state.monsters);
+          }
+        )
+      )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>
+                {monster.name}: {monster.email}
+              </h1>
+              <hr></hr>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
